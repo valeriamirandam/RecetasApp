65f241af03bd0d277f776ac9675f490065890770
RaimundoGross <raimundo.gross@sansano.usm.cl>
2022-10-27 19:52:13 -0300

    Modifica max y min de titulo de receta

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ApplicationRecord
-    validates :title, presence: true, length: {minimum: 2} 
+    validates :title, presence: true, length: {minimum: 3, maximum: 50} 
     validates :ingredients, presence: true, length: {minimum: 5} 
     validates :instructions, presence: true, length: {minimum: 5} 
 end
